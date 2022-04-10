d160f36bf42b30cfae5842cd711b696e3818a846
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-04-10 12:32:30 -0700

    Working login form, also added boilerplate code to react App.js src file to hook Apollo DB into the application.

--- server/server.js
+++ server/server.js
@@ -20,12 +20,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-/** 
-app.use(routes);
-*/
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
 
 
 app.get('/', (req, res) => {
